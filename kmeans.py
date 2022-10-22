cead14e0b1bc261dcea1b496f61b141fa9edb19f
karimkohel <kareemkohel@gmail.com>
2022-10-22 23:44:41 +0200

    starting the class with init and init clusters

--- kmeans.py
+++ kmeans.py
@@ -1,4 +1,15 @@
 import numpy as np
 
 class Kmeans:
-    pass
+    
+    def __init__(self, K, max_iters=100):
+        self.K = K
+        self.max_iterations = max_iters
+        self._init_clusters()
+
+    def _init_clusters(self):
+        self.clusters = [ [] for _ in range(self.K) ]
+        self.centroids = []
+
+    def fit(self, data):
+        pass
