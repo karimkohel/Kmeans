89381c0b01c9660eae9999609c3bf862b882f041
karim kohel <46066647+karimkohel@users.noreply.github.com>
2022-10-29 19:21:16 +0200

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -7,9 +7,6 @@ class Kmeans:
         self.max_iterations = max_iters
         self._init_clusters()
 
-    def _init_clusters(self):
-        self.clusters = [ [] for _ in range(self.K) ]
-        self.centroids = []
 
     def fit(self, data):
         pass
