1f2fdfc5d35f794897fa37931123f7c396af2ad6
karimkohel <kareemkohel@gmail.com>
2022-11-06 01:13:34 +0200

    started the same class again but out of knowledge elmarady

--- kmeans.py
+++ kmeans.py
@@ -1,12 +1,42 @@
+from json.tool import main
+from unicodedata import name
 import numpy as np
+
+np.random.seed(78)
+
+def euclidean_distance(p1, p2):
+    return np.sqrt(np.sum((p1-p2))**2)
+
+
 
 class Kmeans:
     
-    def __init__(self, K, max_iters=100):
+    def __init__(self, K=2, max_iters=100):
         self.K = K
         self.max_iterations = max_iters
         self._init_clusters()
 
+    def _init_clusters(self):
+        self.clusters = [[] for _ in range(self.K)]
+        self.centroids = []
 
-    def fit(self, data):
-        pass
+
+    def predict(self, data):
+        self.data = data
+        self.n_samples, self.n_features = data.shape
+
+        # init random centroids for start
+        randomCentroids = np.random.choice(self.n_samples, self.K, replace=False)
+        self.centroids = [self.data[indx] for indx in randomCentroids]
+
+        # optimize centroids location
+        for _ in range(self.max_iterations):
+            # update data clusters
+            # update centroids
+            # check error
+
+        # return cluster labels
+
+
+if __name__ == '__main__':
+    print("Hello")
